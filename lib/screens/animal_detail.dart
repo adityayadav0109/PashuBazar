4ec7aefa3ed13e7a9e276a7822fbf2609bf115bd
Aditya <adityayadav1743@hmail.com>
2023-05-13 13:14:58 +0530

    slider to make offers implemented

--- lib/screens/animal_detail.dart
+++ lib/screens/animal_detail.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:pashubazar/models/animal.dart';
 
-class AnimalDetailPage extends StatelessWidget {
+class AnimalDetailPage extends StatefulWidget {
   final Animal animal;
 
   const AnimalDetailPage({Key? key, required this.animal}) : super(key: key);
 
   @override
+  _AnimalDetailPageState createState() => _AnimalDetailPageState();
+}
+
+class _AnimalDetailPageState extends State<AnimalDetailPage> {
+  double _selectedPrice = 0;
+
+  @override
   Widget build(BuildContext context) {
+    final double _itemHeight = MediaQuery.of(context).size.height * 0.06;
+    final double _itemWidth = MediaQuery.of(context).size.width * 0.8;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(animal.name),
+        title: Text(widget.animal.name),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -18,23 +28,68 @@ class AnimalDetailPage extends StatelessWidget {
             Container(
               height: 300,
               child: Image.network(
-                animal.imageUrl ??"https://www.cabq.gov/pets/news/before-you-surrender-your-pet/@@images/91c70fd1-adb1-44f8-9f58-8e16bcf64fdc.jpeg",
+                widget.animal.imageUrl ??
+                    'https://www.cabq.gov/pets/news/before-you-surrender-your-pet/@@images/91c70fd1-adb1-44f8-9f58-8e16bcf64fdc.jpeg',
                 fit: BoxFit.cover,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(15.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Price: ${animal.price}',
+                    'Price: ₹${widget.animal.price}',
                     style: TextStyle(fontSize: 20),
                   ),
                   SizedBox(height: 16),
                   Text(
-                    animal.description,
+                    widget.animal.description,
                     style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Select price to offer',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 16),
+                  Slider(
+                    value: _selectedPrice,
+                    min: 0,
+                    max: widget.animal.price.toDouble(),
+                    divisions: 40,
+                    label: '₹${_selectedPrice.toStringAsFixed(2)}',
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPrice = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 32),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Offer made'),
+                              content: Text(
+                                  'Your offer of ₹ ${_selectedPrice.toStringAsFixed(2)} has been made.'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: Text('Make offer'),
+                    ),
                   ),
                 ],
               ),
